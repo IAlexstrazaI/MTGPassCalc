eede2f4d338dd731cc685aae8a6c5041b66b84f9
IAlexstrazaI <nekker1337@gmail.com>
2024-03-06 16:18:51 +0300

    boneworks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>Сегодня: {{ this.Today }}</p>
+    <p>Сегодня: {{ Date(this.Today) }}</p>
     <button @click="timeupdate"> Поставить сегодняшнюю дату  </button>
     <br>
     <br>
     <p>Дата окончания: {{ this.EndDayMP }}</p>
-
+    <p>Максимум lvl: {{ this.MaxLvl }}</p>
     <br>
     <br>
-    Current MP
+    Current MP state
     <br>
     <br>
     Lvl MP:  <input @change = "SaveLocal('CurrentLevel',CurrentLevel)" v-model.number="CurrentLevel">
     <br>
     <br>
     Excess Mastery XP:  <input @change = "SaveLocal('ExcessMasteryExp',ExcessMasteryExp)" v-model.number="ExcessMasteryExp">
+    <br>
+    <br>
+    Incomplete daily wins:  <input @change = "SaveLocal('IncompDW',IncompDW)" v-model.number="IncompDW">
+    <br>
+    <br>
+    Incomplete daily quests:  <input @change = "SaveLocal('IncompDQ',IncompDQ)" v-model.number="IncompDQ">
+    <br>
+    <br>
+    Incomplete weekly wins:  <input @change = "SaveLocal('IncompWW',IncompWW)" v-model.number="IncompWW">
+    <br>
+    <br>
+    Expected mastery progression
+    <br>
+    <br>
+    Daily wins per Day:  <input @change = "SaveLocal('DWperDay',DWperDay)" v-model.number="DWperDay">
+    <br>
+    <br>
+    Daily quests left total:  <input @change = "SaveLocal('DQleft',DQleft)" v-model.number="DQleft">
+    <br>
+    <br>
+    Weekly wins per week:  <input @change = "SaveLocal('WeeklyWinsPerWeek',WeeklyWinsPerWeek)" v-model.number="WeeklyWinsPerWeek">
   </div>
 </template>
 <script>
@@ -39,9 +60,9 @@ export default {
       /* Current MP */
       CurrentLevel: localStorage.getItem('CurrentLevel'),
       ExcessMasteryExp: localStorage.getItem('ExcessMasteryExp'),
-      IcompDW: localStorage.getItem('IncompDW'),
-      IcompDQ: localStorage.getItem('IncompDQ'),
-      IcompWW: localStorage.getItem('IncompWW'),
+      IncompDW: localStorage.getItem('IncompDW'),
+      IncompDQ: localStorage.getItem('IncompDQ'),
+      IncompWW: localStorage.getItem('IncompWW'),
 
       /* Expected mastery progression */
       DWperDay: localStorage.getItem('DWperDay'),
