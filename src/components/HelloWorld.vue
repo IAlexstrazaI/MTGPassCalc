09a5550a891dd38f10f4f8e415d83ec1e4be7378
IAlexstrazaI <nekker1337@gmail.com>
2024-03-11 16:06:07 +0300

    grid layout testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>Сегодня: {{ Date(this.Today) }}</p>
+
+  <h1>{{ msg }}</h1>
+  <div class="col">
+    <p>Сегодня: {{ Date(this.Today)  }}</p>
     <button @click="timeupdate"> Поставить сегодняшнюю дату  </button>
     <br>
     <br>
-    <p>Дата окончания: {{ this.EndDayMP }}</p>
+    <p>Дата окончания: {{ Date(this.EndDayMP) }}</p>
     <p>Максимум lvl: {{ this.MaxLvl }}</p>
     <br>
     <br>
@@ -38,13 +39,41 @@
     <br>
     Weekly wins per week:  <input @change = "SaveLocal('WeeklyWinsPerWeek',WeeklyWinsPerWeek)" v-model.number="WeeklyWinsPerWeek">
   </div>
+  <div class="col" ></div>
+  <div></div>
+
+  <grid-layout :layout.sync="layout"
+               :col-num="12"
+               :row-height="30"
+               :is-draggable="draggable"
+               :is-resizable="resizable"
+               :vertical-compact="true"
+               :use-css-transforms="true"
+  >
+    <grid-item v-for="item in layout"
+               :static="item.static"
+               :x="item.x"
+               :y="item.y"
+               :w="item.w"
+               :h="item.h"
+               :i="item.i"
+    >
+      <span class="text">{{itemTitle(item)}}</span>
+    </grid-item>
+  </grid-layout>
 </template>
 <script>
+
+import VueGridLayout from 'vue-grid-layout';
 window.addEventListener('beforeunload', () => {
 console.log("Loaded!")
 });
 
 export default {
+  components: {
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem
+  },
   name: 'HelloWorld',
   props: {
     msg: String,
@@ -88,6 +117,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 h3 {
   margin: 40px 0 0;
 }
@@ -100,7 +130,7 @@ li {
   margin: 0 10px;
 }
 h1{
- color:  #316e40   ;
+ color:  #316e40 !important;
 }
 
 </style>
